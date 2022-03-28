64af12829842726273cc248ce6c6706c85250366
Jonathan Robinson Manchego Sosa <jonathan571999@gmail.com>
2022-03-28 00:59:30 -0500

    creando migraciones

--- src/common/services/dropbox.js
+++ src/common/services/dropbox.js
@@ -2,7 +2,7 @@ const { Dropbox } = require('dropbox');
 
 const dbx = new Dropbox({
   accessToken:
-    'sl.BEGQPp3hPSMtZqkeiYZd9wyEwiOG4HE6Ka67tkjmfjv0S0CxxuxsqKMeZAlaJkbPN2CFS35AChUqw9GoqdREkbUZQgXjmMn3dloHMEinGzA4c5bugI9tbe9fS8GY5O-yuHR7CTAB3ZDW',
+    'sl.BEorXthxkUoDt_NwBzwYBPVr7h7fx0RfJl8SJuqRHeAQ4x6vkytkJZrCS8qcZkT96EE-r8ud34QjplRf8N8KVNRydp5cTZsTvsqGoOEaogbyOhBkvLKrDxqffWZ9bBnnHBZiHH6j7nnO',
 });
 
 module.exports = dbx;
